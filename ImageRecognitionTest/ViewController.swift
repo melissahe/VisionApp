2ac739cf29d06d0ada4b9066b2f725353b6b5e38
Lisa <imnotlostrui@aim.com>
2018-04-20 23:23:11 -0400

    Initial Commit

---
+++ ImageRecognitionTest/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  ImageRecognitionTest
+//
+//  Created by Lisa J on 4/20/18.
+//  Copyright © 2018 Lisa J. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+class ViewController: UIViewController , AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    let homeView = View()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+        view.addSubview(homeView)
+        homeView.button.addTarget(self, action: #selector(translate), for: .touchUpInside)
+        
+    }
+    
+    func setupCaptureSession() {
+        let captureSession = AVCaptureSession()
+        
+        // search for available capture devices
+        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
+        
+        // setup capture device, add input to our capture session
+        do {
+            if let captureDevice = availableDevices.first {
+                let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
+                captureSession.addInput(captureDeviceInput)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        // setup output, add output to our capture session
+        let captureOutput = AVCaptureVideoDataOutput()
+        captureOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(captureOutput)
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.frame
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+    }
+    
+    // called everytime a frame is captured
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {return}
+        
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+            
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            guard let Observation = results.first else { return }
+            
+            DispatchQueue.main.async(execute: {
+                self.homeView.label.text = "\(Observation.identifier)"
+            })
+        }
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        // executes request
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
+    @objc func translate() {
+        print(homeView.label.text)
+    }
+
+}
+
+
