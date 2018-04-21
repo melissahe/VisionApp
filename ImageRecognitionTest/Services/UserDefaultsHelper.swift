47e609bed3913682fbbce458f7b25f233a36d4c5
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2018-04-21 13:37:41 -0400

    Added Userdefaults for saving language preferences

---
+++ ImageRecognitionTest/Services/UserDefaultsHelper.swift
@@ -0,0 +1,31 @@
+//
+//  UserDefaultsHelper.swift
+//  ImageRecognitionTest
+//
+//  Created by Richard Crichlow on 4/21/18.
+//  Copyright © 2018 Lisa J. All rights reserved.
+//
+
+import Foundation
+struct UserDefaultsHelper {
+    static let manager = UserDefaultsHelper()
+    private init() {}
+    private let baseLanguageKey = "baseLanguageKey"
+    private let targetLanguageKey = "targetLanguageKey"
+    
+    //Get Stuff
+    func getBaseLanguage() -> String? {
+        return UserDefaults.standard.string(forKey: baseLanguageKey)
+    }
+    func getTargetLanguage() -> String? {
+        return UserDefaults.standard.string(forKey: targetLanguageKey)
+    }
+    
+    //Save Stuff
+    func setBaseLanguage(to newZipcode: String) {
+        UserDefaults.standard.setValue(newZipcode, forKey: baseLanguageKey)
+    }
+    func setTargetLangauge(to newZipcode: String) {
+        UserDefaults.standard.setValue(newZipcode, forKey: targetLanguageKey)
+    }
+}
